7849ee85852ba7f02229aa7fb0596cd920ddbeab
Gowtham <nagamallagowtham@gmail.com>
2026-01-07 13:16:49 +0530

    remove element, reverse string & buy ans sell stocks

---
+++ arrays/reverseString.js
@@ -0,0 +1,22 @@
+// Reverse String 
+
+// Write a function that reverses a string. The input string is given as an array of characters s.
+
+// You must do this by modifying the input array in-place with O(1) extra memory..
+
+var reverseString = function(s) {
+    let len = s.length;
+    let halfLen = Math.floor(len / 2);
+
+    for (let i = 0; i < halfLen; i++) {
+        let temp = s[i];
+        s[i] = s[len-(i + 1)];
+        s[len-(i + 1)] = temp;
+    }
+}
+
+let string = ["h", "e", "l", "l", "o"];
+
+reverseString(string);
+
+console.log(string);
