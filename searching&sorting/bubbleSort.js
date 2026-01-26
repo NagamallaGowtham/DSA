7ca7f616a0c76bba30f2caf781095c3345f4040d
Gowtham <nagamallagowtham@gmail.com>
2026-01-26 16:29:30 +0530

    searching & sorting revision

--- searching&sorting/bubbleSort.js
+++ searching&sorting/bubbleSort.js
@@ -2,22 +2,20 @@ let array = [20, 10, 4, 9, 34, 2, 7, 6];
 
 var bubbleSort = function(arr) {
     let n = arr.length;
-
-    let isSwapped = false;
-
+    
     for (let i = 0; i < n - 1; i++) {
+        let isSwapped = false;
         for (let j = 0; j < n - 1 - i; j++) {
             if (arr[j] > arr[j+1]) {
                 let temp = arr[j];
                 arr[j] = arr[j+1];
-                arr[j+1] = temp;
+                arr[j+1] = temp; 
 
                 isSwapped = true;
             }
         }
         if (!isSwapped) break;
     }
-
 
     return arr;
 }
