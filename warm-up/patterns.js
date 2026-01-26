8480237f6e6a4b0986c6da122b63c3c222973762
Gowtham <nagamallagowtham@gmail.com>
2026-01-26 16:55:02 +0530

    star patterns revision

--- warm-up/patterns.js
+++ warm-up/patterns.js
@@ -122,6 +122,18 @@ for (let i = 0; i < n; i++) {
     console.log(row);
 }
 
+for (let i = 0; i < n; i++) {
+    let row = "";
+    for (let j = 0; j < i + 1; j++) {
+        if (j % 2 == 0) {
+            row = row + 1;
+        } else {
+            row = row + 0;
+        }
+    }
+    console.log(row);
+}
+
 /* 
     1
     01
